65741629470c6b3f18fb198287f89b252c472a82
nodev918 <devnode77@gmail.com>
2026-04-29 21:08:32 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/five-star-lord/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
